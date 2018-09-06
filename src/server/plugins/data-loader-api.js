a1eef23e4a62a8d8912a32bfa67d86206678e509
Gokul Kavaturi <gkavaturi@walmartlabs.com>
2018-09-06 01:16:02 -0700

    data api: adding cache support for storing keyword to product id mapping

--- src/server/plugins/data-loader-api.js
+++ src/server/plugins/data-loader-api.js
@@ -1,34 +1,18 @@
-import csv from "csvtojson";
 import Promise from "bluebird";
 import fetch from "node-fetch";
+import Catbox from "catbox";
+import catboxMemory from "catbox-memory";
 import _get from "lodash/get";
 import _flatten from "lodash/flatten";
 import _mergeWith from "lodash/mergeWith";
-import WordPOS from "wordpos";
+import WordPos from "wordpos";
+import defaultProductIds from "../data/sample-ids.json";
 
+//create artifical delay to go around product api rate limiting
+const TIMER_INVERVAL = 500;
+const DATA_MISSING_ERR_MSG = "No data found";
 const PRODUCT_API_URL = "http://api.walmartlabs.com/v1/items/";
 const API_KEY = "kjybrqfdgp3u4yv2qzcnjndj";
-
-const itemIds = [
-  14225185,
-  14225186,
-  14225188,
-  14225187,
-  39082884,
-  30146244,
-  12662817,
-  34890820,
-  19716431,
-  42391766,
-  35813552,
-  40611708,
-  40611825,
-  36248492,
-  44109840,
-  23117408,
-  35613901,
-  42248076
-]
 
 class DataLoaderApi {
   constructor() {
@@ -42,8 +26,11 @@ class DataLoaderApi {
       name: "FileLoaderApi",
       version: "1.0.0"
     };
-    this.productData = [];
-    this.wordpos = new WordPOS();
+    this.wordpos = new WordPos();
+    this.cache = new Catbox.Client(catboxMemory,{ partition: 'keywords'});
+    this.cache.start().then(() => {
+      console.log("Cache started");
+    });
   }
 
   register(server, options, next) {
@@ -93,9 +80,12 @@ class DataLoaderApi {
     });
   }
 
+  /*
+  Parse data into keywords, product id pairs
+  */
   parseData(values) {
     const flatValues = [].concat(values);
-  
+    
     return flatValues.reduce((parentStore, keywords) => {
       const itemId = Object.keys(keywords)[0];
       const values = keywords[itemId].reduce((localStore, localWord) => {
@@ -120,7 +110,7 @@ class DataLoaderApi {
 
   loadData(request, reply) {
     const productPromises = [];
-    itemIds.forEach((itemId, index) => {
+    defaultProductIds.forEach((itemId, index) => {
         const productPromise = new Promise((resolve, reject) => {
           setTimeout(() => {
             return fetch(`${PRODUCT_API_URL}${itemId}?format=json&apiKey=${API_KEY}`)
@@ -130,26 +120,44 @@ class DataLoaderApi {
                 return null;
               }).catch((err) => {
                 reject(err);
+                return null;
               });
-          }, 500*index);  
+          }, TIMER_INVERVAL*index);  
         });
         productPromises.push(productPromise);
     });
 
     Promise.all(productPromises)
       .then(function(values){
-        this.productData = this.parseData(values);
-        reply(this.productData);
+        const productData = this.parseData(values);
+        //set 1 day cache for product data
+        this.cache
+          .set({segment: "keywords", id: "keywordStore"}, productData, 86400000)
+          .then(() => { 
+            console.log("Cache set for keyword store");
+            reply(productData);
+          })
+          .catch((err) => {
+            reply(`Unable to set cache for keyword store ${err}`);
+          });
+        
       }.bind(this))
       .catch(function(error) { console.log(error); });
   }
 
-  getProductsByKeyword(request, reply) {
+  async getProductsByKeyword(request, reply) {
     const keywords = request.query.keywords || "";
+    const productData = await this.cache.get({segment: "keywords", id: "keywordStore"});
+    
+    if (!productData) {
+      reply(DATA_MISSING_ERR_MSG);
+    }
+
     const productIds = keywords.split(",").map((keyword) => {
-      return this.productData[keyword];
-    })
-    const mergedProductsIds = _flatten(productIds).reduce((productIdStore, productId) => {
+      return productData.item[keyword];
+    });
+
+    const mergedProductsIds = [].concat(productIds).reduce((productIdStore, productId) => {
       if (!productIdStore.includes(productId)) {
         return productIdStore.concat(productId)
       } else {
